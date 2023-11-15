0a0cee94ebded3fc19d598ae8a7de7ed3b9142bf
Philipp Bayer <philippbay@gmail.com>
2023-11-15 12:59:47 +0800

    Ran styler

--- R/main.R
+++ R/main.R
@@ -16,32 +16,35 @@ NULL
 #'        paste0(rappdirs::app_dir("aquamaps")$config(), '/am.db')
 #' @export
 #' @examples
-#' shp <- get_shapes('some_examples.rds')
+#' shp <- get_shapes("some_examples.rds")
 get_shapes <- function(rds,
                        read_cutoff = 10,
-                       db_file = paste0(rappdirs::app_dir("aquamaps")$config(), '/am.db')) {
+                       db_file = paste0(rappdirs::app_dir("aquamaps")$config(), "/am.db")) {
   con <- DBI::dbConnect(RSQLite::SQLite(), db_file)
 
   a <- readRDS(rds)
 
   asvs_to_location <-
-    a@otu_table |> as.data.frame() |>
-    dplyr::as_tibble(rownames = 'ASV') |>
-    tidyr::pivot_longer(-ASV, names_to = 'Location', values_to = 'Reads') |>
+    a@otu_table |>
+    as.data.frame() |>
+    dplyr::as_tibble(rownames = "ASV") |>
+    tidyr::pivot_longer(-ASV, names_to = "Location", values_to = "Reads") |>
     dplyr::filter(Reads > 0)
 
-  asvs_to_species <- a@tax_table |> as.data.frame() |>
-    tidyr::as_tibble(rownames = 'ASV') |>
+  asvs_to_species <- a@tax_table |>
+    as.data.frame() |>
+    tidyr::as_tibble(rownames = "ASV") |>
     dplyr::select(ASV, species) |>
-    dplyr::filter(species != 'dropped')
+    dplyr::filter(species != "dropped")
 
-  location_to_latlong <- a@sam_data |> as.data.frame() |>
-    tidyr::as_tibble(rownames = 'Location') |>
+  location_to_latlong <- a@sam_data |>
+    as.data.frame() |>
+    tidyr::as_tibble(rownames = "Location") |>
     dplyr::select(Location, latitude_dd, longitude_dd)
 
   asvs_to_species_and_location <- asvs_to_species |>
-    dplyr::left_join(asvs_to_location, by ='ASV') |>
-    dplyr::left_join(location_to_latlong, by = 'Location') |>
+    dplyr::left_join(asvs_to_location, by = "ASV") |>
+    dplyr::left_join(location_to_latlong, by = "Location") |>
     dplyr::filter(!is.na(latitude_dd)) |>
     dplyr::filter(Reads > read_cutoff)
 
@@ -52,7 +55,7 @@ get_shapes <- function(rds,
 
   # have a look
   all_species <- con |>
-    dplyr::tbl(dplyr::sql('select * from speciesoccursum_r')) |>
+    dplyr::tbl(dplyr::sql("select * from speciesoccursum_r")) |>
     dplyr::collect() |>
     dplyr::mutate(full_species = paste(Genus, Species)) |>
     dplyr::filter(full_species %in% all_species_seen)
@@ -62,35 +65,39 @@ get_shapes <- function(rds,
   all_c_squares <- con %>%
     dplyr::tbl("hcaf_species_native") %>%
     dplyr::filter(SpeciesID %in% !!all_species$SpeciesID) |>
-    dplyr::left_join(con |> dplyr::tbl('hcaf_r') |> dplyr::select(CsquareCode, NLimit, Slimit, WLimit, ELimit), by = 'CsquareCode') |>
+    dplyr::left_join(con |> dplyr::tbl("hcaf_r") |> dplyr::select(CsquareCode, NLimit, Slimit, WLimit, ELimit), by = "CsquareCode") |>
     dplyr::collect() %>%
     dplyr::select(
       SpeciesID, CsquareCode,
       NLimit, Slimit, WLimit, ELimit,
-      Probability)
+      Probability
+    )
 
   # now we have the squares; see whether our sightings overlap with these squares
   # this is a bit of a hack, but it works
 
   asvs_to_aquamaps_prob <- asvs_to_species_and_location |>
     # need to get the aquamaps species ID
-    dplyr::left_join(all_species |> dplyr::select(SpeciesID, full_species), by = c('species' = 'full_species')) |>
+    dplyr::left_join(all_species |> dplyr::select(SpeciesID, full_species), by = c("species" = "full_species")) |>
     # now for each species, get all squares for that species (there's a nicer way of doing this)
-    dplyr::left_join(all_c_squares, by  = c('SpeciesID'), relationship = "many-to-many") |>
+    dplyr::left_join(all_c_squares, by = c("SpeciesID"), relationship = "many-to-many") |>
     # now keep only the aquamaps squares that fit with the sighting latitude/longitude
     dplyr::filter(latitude_dd > Slimit, latitude_dd < NLimit, longitude_dd > WLimit, longitude_dd < ELimit)
 
   # we need the points of ASVs that don't have any overlap
   missing_asvs <- asvs_to_species_and_location |>
-    dplyr::filter(! ASV %in% asvs_to_aquamaps_prob$ASV) |>
-    dplyr::left_join(all_species |> dplyr::select(SpeciesID, full_species), by = c('species' = 'full_species'))
+    dplyr::filter(!ASV %in% asvs_to_aquamaps_prob$ASV) |>
+    dplyr::left_join(all_species |> dplyr::select(SpeciesID, full_species), by = c("species" = "full_species"))
 
   # put the two tibbles together and return
   both <- dplyr::bind_rows(missing_asvs, asvs_to_aquamaps_prob)
-  both <- both |> dplyr::mutate(Probability_class = dplyr::case_when(Probability > 0.99 ~ 'Great (>0.99)',
-                                                       Probability > 0.5 ~ 'Good (>0.5)',
-                                                       Probability > 0 ~ 'OK (>0)')) |>
-    dplyr::mutate(Probability_class = factor(Probability_class, levels = c('Great (>0.99)', 'Good (>0.5)', 'OK (>0)')))
+  both <- both |>
+    dplyr::mutate(Probability_class = dplyr::case_when(
+      Probability > 0.99 ~ "Great (>0.99)",
+      Probability > 0.5 ~ "Good (>0.5)",
+      Probability > 0 ~ "OK (>0)"
+    )) |>
+    dplyr::mutate(Probability_class = factor(Probability_class, levels = c("Great (>0.99)", "Good (>0.5)", "OK (>0)")))
 
   DBI::dbDisconnect(con)
 
@@ -105,15 +112,19 @@ get_shapes <- function(rds,
 #' @param subset A vector with the names of ASVs to plot. Defaults to 10 at random that have a Probability.
 #' @export
 #' @examples
-#' plot_shapes(get_shapes('some_examples.rds'))
-plot_shapes <- function(table, subset = table |> dplyr::filter(!is.na(Probability)) |> dplyr::pull(ASV) |> unique()|> sample(10)) {
+#' plot_shapes(get_shapes("some_examples.rds"))
+plot_shapes <- function(table, subset = table |>
+                          dplyr::filter(!is.na(Probability)) |>
+                          dplyr::pull(ASV) |>
+                          unique() |>
+                          sample(10)) {
   my_sf <- sf::st_as_sf(table |>
-                      dplyr::filter(ASV %in% subset), coords = c('longitude_dd', 'latitude_dd'))
+    dplyr::filter(ASV %in% subset), coords = c("longitude_dd", "latitude_dd"))
 
   p <- ggplot(my_sf) +
-    geom_sf(aes(color=log(Reads), shape = Probability_class), size = 3) +
-    facet_wrap(~ASV+species) +
-    labs(shape="Probability class")
+    geom_sf(aes(color = log(Reads), shape = Probability_class), size = 3) +
+    facet_wrap(~ ASV + species) +
+    labs(shape = "Probability class")
 
   p
 }
--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,6 @@
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("This is version ", utils::packageVersion(pkgname),
-                        " of ", pkgname, '. If you have not done so, please run download_data() to download the Aquamaps database.')
+  packageStartupMessage(
+    "This is version ", utils::packageVersion(pkgname),
+    " of ", pkgname, ". If you have not done so, please run download_data() to download the Aquamaps database."
+  )
 }
